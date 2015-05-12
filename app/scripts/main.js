7bddbb986b66c6a205e1f4c3d505cf238b7ce348
Joshua Tuscan <hello@joshuatuscan.com>
2015-05-11 19:45:27 -0700

    going live

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 /* jshint devel:true */
 
+'use strict';
 
 //validation functions
 
@@ -51,18 +52,18 @@ function register($form) {
         data: $form.serialize(),
         cache       : false,
         dataType    : 'json',
-        contentType: "application/json; charset=utf-8",
-        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+        contentType: 'application/json; charset=utf-8',
+        error       : function(err) { alert('Could not connect to the registration server. Please try again later.'); },
         success     : function(data) {
-            if (data.result != "success") {
+            if (data.result !== 'success') {
                 // Something went wrong, do something to notify the user. maybe alert(data.msg);
-                console.log("failure :( ");
+                console.log('failure :( ');
             } else {
                 // It worked, carry on...
-                console.log("success!!!!");
+                console.log('success!!!!');
                 $('#signupForm').fadeOut(100, function() {
                     $('.success-message').fadeIn(300);
-                })
+                });
             }
         }
     });
@@ -80,15 +81,13 @@ function checkEmail() {
 }
 
 
-
-
 $(document).ready( function () {
     // I only have one form on the page but you can be more specific if need be.
     var $form = $('form');
 
     if ( $form.length > 0 ) {
         $('form input[type="submit"]').bind('click', function ( event ) {
-            if ( event ) event.preventDefault();
+            if ( event ) {event.preventDefault()};
             // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
             if ( checkEmail() === true ) { 
                 register($form); 

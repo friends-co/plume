ac31a8f66038e9a8f0d59cbbfc21e4bee23d4f53
Joshua Tuscan <hello@joshuatuscan.com>
2015-05-11 18:59:16 -0700

    working newsletter signup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,104 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+
+
+//validation functions
+
+var validateForm = {
+    isValidEmailAddress: function(emailAddress) {
+        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        return pattern.test(emailAddress);
+    },
+    isRequired: function(value) {
+        if (value === '' || value === null) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+    isValidURL: function(url) {
+        var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+        return re.test(url);
+    },
+    isValidSelect: function(value) {
+        if (value === 'default') {
+            return false;
+        } else {
+            return true;
+        }
+    },
+    isValidChecked: function(checkbox) {
+        var parentBox = $(checkbox).closest('.checkbox-block');
+
+        if ($('input[type="checkbox"]',parentBox).is(':checked')) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    isMatching: function(value1, value2) {
+        if ( value1 === value2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+function register($form) {
+    $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache       : false,
+        dataType    : 'json',
+        contentType: "application/json; charset=utf-8",
+        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+        success     : function(data) {
+            if (data.result != "success") {
+                // Something went wrong, do something to notify the user. maybe alert(data.msg);
+                console.log("failure :( ");
+            } else {
+                // It worked, carry on...
+                console.log("success!!!!");
+                $('#signupForm').fadeOut(100, function() {
+                    $('.success-message').fadeIn(300);
+                })
+            }
+        }
+    });
+}
+
+function checkEmail() {
+    var emailAddy = $('#mce-EMAIL').val();
+    console.log(emailAddy);
+
+    if( !validateForm.isValidEmailAddress(emailAddy) ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+
+
+$(document).ready( function () {
+    // I only have one form on the page but you can be more specific if need be.
+    var $form = $('form');
+
+    if ( $form.length > 0 ) {
+        $('form input[type="submit"]').bind('click', function ( event ) {
+            if ( event ) event.preventDefault();
+            // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
+            if ( checkEmail() === true ) { 
+                register($form); 
+            } else {
+                $('.fail-message').fadeIn(300);
+                setTimeout(function() {
+                    $('.fail-message').fadeOut(300);
+                }, 3000);
+            }
+        });
+    }
+
+});
